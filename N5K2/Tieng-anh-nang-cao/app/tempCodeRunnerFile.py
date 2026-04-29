2fb36e7c897c7c397f11afc8540d80ef17e1a8a5
Annguyen011 <nguyenvanan01102003@gmail.com>
2026-04-29 16:26:18 +0700

    Refactor code structure for improved readability and maintainability

--- N5K2/Tieng-anh-nang-cao/app/tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DB_PATH = os.path.join(BASE_DIR, "vocab.db")
-CACHE_DIR = os.path.join(BASE_DIR, "image_cache")
-AUDIO_CACHE_DIR = os.path.join(BASE_DIR, "audio_cache")
-os.makedirs(CACHE_DIR, exist_ok=True)
-os.makedirs(AUDIO_CACHE_DIR, exist_ok=True)
